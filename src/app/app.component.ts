4b067940bb751072b175ac4992f28e33d893d3dc
Galih Laras Prakoso <galih.laras@pass3.id>
2024-04-25 08:48:02 +0700

    Upload File Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-upload-file';
+
+  selectedFile: File | null = null
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0]
+  }
+
+  uploadFile() {
+    if (!this.selectedFile) {
+      alert('No file selected')
+      return;
+    }
+
+    const formData = new FormData()
+    formData.append('image', this.selectedFile)
+
+    this.http.post<any>('http://api.imgbb.com/1/upload', formData)
+    .pipe(
+      switchMap(response => {
+        alert(`Upload successful: ${JSON.stringify(response)}`)
+        return response
+      })
+    ).subscribe(
+      result => {
+        alert(`Upload successful: ${JSON.stringify(result)}`)
+      },
+      err => {
+        alert(`Upload error: ${err}`)
+      }
+    )
+  }
 }
